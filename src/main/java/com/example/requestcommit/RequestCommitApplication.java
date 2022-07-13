b9bda97275e196ce79ab4a8e85711019a755b3e5
KSVoss <Kai.S.Voss@t-online.de>
2022-07-13 15:51:26 +0200

    Initial Commit

---
+++ src/main/java/com/example/requestcommit/RequestCommitApplication.java
@@ -0,0 +1,13 @@
+package com.example.requestcommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestCommitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequestCommitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/requestcommit/RequestCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.requestcommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestCommitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
